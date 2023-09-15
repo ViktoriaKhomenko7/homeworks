c4c53040e1a70899ee6131e55752ff93e19113db
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-09-15 17:06:27 +0300

    do homework

---
+++ src/site/NewComponent.tsx
@@ -0,0 +1,55 @@
+type NewComponentPropsType = {
+    topCars: topCarsType[]
+}
+
+type topCarsType = {
+    manufacturer: string,
+    model: string
+}
+export const NewComponent = (props: NewComponentPropsType) => {
+
+
+
+    // const rows = props.topCars.map((objectFromCarArray, index) => {
+    //     return (
+    //         <tr key={objectFromCarArray.manufacturer}>
+    //             <td>{objectFromCarArray.manufacturer}</td>
+    //             <td>{objectFromCarArray.model}</td>
+    //         </tr>
+    //     )
+    // });
+
+
+
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <td>Марка</td>
+                    <td>Модель</td>
+                </tr>
+                </thead>
+                <tbody>
+                {props.topCars.map((objectFromCarArray, index) => <tr key={objectFromCarArray.manufacturer}>
+                                 <td>{objectFromCarArray.manufacturer}</td>
+                                 <td>{objectFromCarArray.model}</td>
+                             </tr>
+                         )
+                     }
+                </tbody>
+            </table>
+        )
+
+};
+
+
+// <ul>
+//     {props.topCars.map((objectFromCarArray, index) => {
+//         return (
+//             <li key={objectFromCarArray.manufacturer}>
+//                 <span>{objectFromCarArray.manufacturer}</span>
+//                 <span> model: {objectFromCarArray.model}</span>
+//             </li>
+//         )
+//     })}
+// </ul>
